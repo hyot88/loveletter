00169d2455c3aa83a82b43670118ab5bd72eaf71
hyot.ahn <hyot.ahn@kakaoenterprise.com>
2025-12-01 14:48:13 +0900

    init

---
+++ src/main/java/com/simiyami/loveletter/LoveletterApplication.java
@@ -0,0 +1,13 @@
+package com.simiyami.loveletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveletterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveletterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simiyami/loveletter/LoveletterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simiyami.loveletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveletterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
